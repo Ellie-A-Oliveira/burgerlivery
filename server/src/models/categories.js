08474b58a8cacf4c65f836d0fe0fc8313e267e5e
Ellie de Oliveira <ellie.a.oliveira@gmail.com>
2024-03-17 11:36:12 -0300

    chore: transform in monorepo

---
+++ server/src/models/categories.js
@@ -0,0 +1,9 @@
+const categories = [
+  { text: "Entradinhas", link: "/appetizers" },
+  { text: "Burgers", link: "/hamburgers" },
+  { text: "Combos", link: "/combos" },
+  { text: "Sobremesas", link: "/desserts" },
+  { text: "Bebidas", link: "/beverages" },
+];
+
+module.exports = categories;
